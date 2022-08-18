e0b1bd4873d8fe53045e02f074351b4e246bbe4a
DEBJIT4455 <111341137+DEBJIT4455@users.noreply.github.com>
2022-08-18 15:39:20 +0530

    Add files via upload

---
+++ inheritance.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSPROJECT
+{
+    public class inheritance
+    {
+         class old_phone
+        {
+            public void Display()
+            {
+                Console.WriteLine("Display is present");
+            }
+            public void Communication()
+            {
+                Console.WriteLine("Communication is present");
+            }
+            public void MusicPlayer()
+            {
+                Console.WriteLine("MusicPlayer is There");
+            }
+        }
+         class smartphone : old_phone
+        {
+            public void Camera()
+            {
+                Console.WriteLine("camera is Added to the smartphone");
+            }
+            public void Sensor()
+            {
+                Console.WriteLine("Sensor is present in the  smartphone");
+            }
+            static void Main()
+            {
+                smartphone sm = new smartphone();
+                Console.WriteLine("Old features are");
+                sm.Display();
+                sm.Communication();
+                sm.MusicPlayer();
+                Console.WriteLine("--------------");
+                Console.WriteLine("New features are");
+                sm.Camera();
+                sm.Camera();
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
